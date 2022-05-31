dd7030be2b0d12f6bd4b676c728af419dbd3ba2e
Erik Mende <emende@student.hive.fi>
2022-05-31 17:51:39 +0300

    Started making navbar

---
+++ header.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="css/header.css">
+<header>
+	<nav class="navbar">
+		<div class="menu">
+			<img class="logo" src="images/logo.svg" alt="logo">
+			<ul class="nav-links">
+				<li><i class="ti ti-home" id="house"></i></li>
+				<li><i class="ti ti-send"></i></li>
+				<li><i class="ti ti-camera-plus"></i></li>
+				<li><i class="fa-regular fa-compass"></i></li>
+				<li><i class="fa-regular fa-heart"></i></li>
+			</ul>
+		</div>
+	</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Camagru</title>
+		<link rel="stylesheet" href="https://unpkg.com/@tabler/icons@latest/iconfont/tabler-icons.min.css">
+		<script src="https://kit.fontawesome.com/c0a2ce9299.js" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/header.css">
+		<link rel="icon" type="image/x-icon" href="images/favicon.png">
+	</head>
+	<body>
+		<?php include_once "header.php" ?>
+	</body>
+</html>
