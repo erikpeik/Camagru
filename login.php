1ab07408495a367f2248eb0e281b3e66363b06c6
Erik Mende <emende@student.hive.fi>
2022-06-02 21:14:25 +0300

    Styling login.php with some CSS

--- login.php
+++ login.php
@@ -6,18 +6,20 @@
 		<link rel="stylesheet" href="https://unpkg.com/@tabler/icons@latest/iconfont/tabler-icons.min.css">
 		<script src="https://kit.fontawesome.com/c0a2ce9299.js" crossorigin="anonymous"></script>
 		<link rel="stylesheet" href="css/style.css">
-		<link rel="stylesheet" href="css/header.css">
+		<link rel="stylesheet" href="css/login.css">
 		<link rel="icon" type="image/x-icon" href="images/favicon.png">
 	</head>
 	<body>
 		<?php include_once "header.php" ?>
-		<section class="signup-form">
-			<h2>Login</h2>
-			<form action="includes/login-inc.php" method='post'>
-				<input type='text' name='name' placeholder="Username/Email" autocomplete="username" required>
-				<input type='password' name='pwd' placeholder="Password" autocomplete="current-password" required>
-				<button type='submit' name='submit'>Login</button>
-			</form>
-		</section>
+		<div class='box'>
+			<section class="signup-form">
+				<img class="login_logo" src="images/logo.svg" alt="logo">
+				<form action="includes/login-inc.php" method='post'>
+					<input class='login_input' type='text' name='name' placeholder="Username/Email" autocomplete="username" required>
+					<input class='login_input' type='password' name='pwd' placeholder="Password" autocomplete="current-password" required>
+					<button class='login_button'type='submit' name='submit'>Log In</button>
+				</form>
+			</section>
+		</div>
 	</body>
 </html>
