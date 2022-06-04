6016eda98009a56b2a5f2fa917d36861e4eea0c9
Erik Mende <emende@student.hive.fi>
2022-06-04 14:42:48 +0300

    Added camera page, changed header and CSS

--- camera.php
+++ camera.php
@@ -1,19 +1,8 @@
-<?php
-session_start();
-if (!isset($_SESSION["user_id"])) {
-	header("Location: login.php");
-}
-if (isset($_GET['logout'])) {
-	unset($_SESSION["user_id"]);
-	unset($_SESSION["user_uid"]);
-	header("Location: login.php");
-}
-?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Camagru</title>
+		<title>Camera • Camagru</title>
 		<link rel="stylesheet" href="https://unpkg.com/@tabler/icons@latest/iconfont/tabler-icons.min.css">
 		<script src="https://kit.fontawesome.com/c0a2ce9299.js" crossorigin="anonymous"></script>
 		<link rel="stylesheet" href="css/style.css">
--- header.php
+++ header.php
@@ -5,8 +5,10 @@
 	<nav class="navbar">
 		<img class="logo" src="images/logo.svg" alt="logo" onclick="location.href = 'index.php'">
 		<ul class="nav-links">
-			<li id='user-icon' class="ti ti-user"></i></li>
+			<li id='logout-icon' title='Home' onclick="location.href = 'index.php'"><i class="ti ti-home"></i></li>
+			<li id='logout-icon' title='Camera' onclick="location.href = 'camera.php'"><i class="ti ti-camera-plus"></i></li>
 			<li id='logout-icon' title='Logout' onclick="location.href = 'index.php?logout=true'"><i class="ti ti-door-exit"></i></li>
+			<li id='user-icon'><i class="ti ti-user"><span id='name-text'><?php echo($_SESSION['user_uid']); ?></span></i></li>
 		</ul>
 
 	</nav>
