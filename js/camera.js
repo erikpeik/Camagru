7735534bf155863487a17dcb2321dadb497763ba
Erik Mende <emende@student.hive.fi>
2022-06-11 20:12:16 +0300

    Fliped Image after taking picture

--- js/camera.js
+++ js/camera.js
@@ -2,6 +2,7 @@ let camera_button = document.querySelector("#start-camera");
 let video = document.querySelector("#video");
 let click_button = document.querySelector("#click-photo");
 let canvas = document.querySelector("#canvas");
+let ctx = canvas.getContext('2d');
 
 camera_button.addEventListener('click', async function() {
 	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
@@ -9,7 +10,9 @@ camera_button.addEventListener('click', async function() {
 });
 
 click_button.addEventListener('click', function() {
-	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+	ctx.translate(canvas.width, 0);
+	ctx.scale(-1, 1);
+	ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 	let image_data_url = canvas.toDataURL('image/jpeg');
 
 	// data url of the image
