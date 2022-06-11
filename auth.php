390110b66715f56d16736f18ab8bc05f07ccb865
Erik Mende <emende@student.hive.fi>
2022-06-11 18:25:35 +0300

    Activation works, but message is wrong

--- auth.php
+++ auth.php
@@ -10,6 +10,7 @@ if (isset($_GET['email']) && isset($_GET['activation_code'])) {
 	$user = find_unverified_user($_GET['activation_code'], $_GET['email'], $pdo);
 	if ($user && activate_user($user[0]['users_id'], $pdo)) {
 		header("Location: login.php?error=account_activated");
+	} else {
+		header("Location: signup.php?error=activation_link_not_valid");
 	}
-	header("Location: signup.php?error=activation_link_not_valid");
 }
