0ab0a968528bffbda906d08a45091adbb54699d1
Erik Mende <emende@student.hive.fi>
2022-07-27 20:18:09 +0300

    Trying if this are unnecesary checks

--- js/camera.js
+++ js/camera.js
@@ -93,15 +93,15 @@ click_button.addEventListener('click', function() {
 		// console.log("Sticker height: " + check[i].offsetHeight);
 		var left = parseInt(check[i].style.left, 10) * multiplier;
 		left = Math.floor(left);
-		if (left > (video.offsetWidth * multiplier) - (check[i].offsetWidth * multiplier)) {
-			left = (video.offsetWidth * multiplier) - (check[i].offsetWidth * multiplier);
-		}
+		// if (left > (video.offsetWidth * multiplier) - (check[i].offsetWidth * multiplier)) {
+		// 	left = (video.offsetWidth * multiplier) - (check[i].offsetWidth * multiplier);
+		// }
 
 		var top = parseInt(check[i].style.top, 10) * multiplier;
 		top = Math.floor(top);
-		if (top > (video.offsetHeight * multiplier) - (check[i].offsetHeight * multiplier)) {
-			top = (video.offsetHeight * multiplier) - (check[i].offsetHeight * multiplier);
-		}
+		// if (top > (video.offsetHeight * multiplier) - (check[i].offsetHeight * multiplier)) {
+		// 	top = (video.offsetHeight * multiplier) - (check[i].offsetHeight * multiplier);
+		// }
 
 		var width = check[i].offsetWidth * multiplier;
 		width = Math.floor(width);
