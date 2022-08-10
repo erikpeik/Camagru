da4d53b173a2cb75dc43a905000f96b7bf3272c3
Erik Mende <emende@student.hive.fi>
2022-08-10 20:36:38 +0300

    It also deletes other users comments before removing pictures

--- includes/delete_account.php
+++ includes/delete_account.php
@@ -8,6 +8,22 @@ if (!isset($_SESSION)) {
 if (isset($_SESSION['user_id']) && $_SESSION['user_id'] != -1) {
 	require_once '../config/pdo.php';
 	try {
+		$sql = "SELECT `image_id` FROM `images` WHERE `users_id` = ?";
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute([$_SESSION['user_id']]);
+		$images = $stmt->fetchAll();
+
+		foreach ($images as $image) {
+			$image_id = $image['image_id'];
+			$sql = "DELETE FROM `comments` WHERE `image_id` = ?";
+			$stmt = $pdo->prepare($sql);
+			$stmt->execute([$image_id]);
+
+			$sql = "DELETE FROM `likes` WHERE `image_id` = ?";
+			$stmt = $pdo->prepare($sql);
+			$stmt->execute([$image_id]);
+		}
+
 		$sql = "DELETE FROM `comments` WHERE `users_id` = ?;";
 		$stmt = $pdo->prepare($sql);
 		$stmt->execute([$_SESSION['user_id']]);
